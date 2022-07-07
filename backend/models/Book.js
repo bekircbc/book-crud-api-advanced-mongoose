aab6fdfcbfe900ac9b7fc28e028aaa1095da5f7d
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-07 11:24:29 +0200

    new schema types added..

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,12 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const bookSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  numberOfPages: Number,
-  language: String,
-  imageUrl: String,
-  buyUrl: String,
+	title: String,
+	description: String,
+	numberOfPages: Number,
+	language: String,
+	imageUrl: String,
+	buyUrl: String,
+	whenPurchased: Date,
+	relatedBook: mongoose.SchemaTypes.ObjectId,
+	topics: [String],
+	author: {
+		firstName: String,
+		lastName: String,
+	},
 });
 
-export const Book = mongoose.model("book", bookSchema);
+export const Book = mongoose.model('book', bookSchema);
